88cde3880d9cd8c39fb6b212ed97d1a604dabc14
Carlos Martínez <ced.mendoza@gmail.com>
2023-11-29 18:43:44 -0600

    main: Change database from decimal to fraction and show fractional units

--- src/components/FoodGroupEntry.svelte
+++ src/components/FoodGroupEntry.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Fraction from "fraction.js";
+
     export let entry;
     export let foodGroups;
     export let ingredients;
@@ -8,7 +10,7 @@
 </script>
 
 <div class="collapse bg-base-200">
-    <input type="checkbox" name="food-group-entry-accordion-{entry.group}" />
+    <input type="checkbox" name="food-group-entry-accordion-{entry.group}"/>
     <div class="collapse-title text-xl text-md">
         <div class="flex items-center justify-center flex-row">
             <i class="ti ti-{foodGroup.icon}"></i>&nbsp;
@@ -16,11 +18,24 @@
             <div class="flex-none text-sm">{entry.points} puntos</div>
         </div>
     </div>
-    <div class="collapse-content">
-        <ul>
-        {#each foodGroupIngredients as ingredient}
-            <li>{ingredient.name} {ingredient.point_exchange} {ingredient.unit}</li>
-        {/each}
-        </ul>
+    <div class="collapse-content px-10">
+        <table class="table-auto w-full my-2">
+            <thead>
+                <tr>
+                    <th class="text-left">Ingrediente</th>
+                    <th class="text-right w-28 max-w-28">Punto</th>
+                    <th class="text-right w-28 max-w-28">Porción</th>
+                </tr>
+            </thead>
+            <tbody>
+            {#each foodGroupIngredients as {name, point_exchange, unit}}
+                <tr>
+                    <td class="text-left">{name}</td>
+                    <td class="text-right">{point_exchange} {unit}</td>
+                    <td class="text-right">{new Fraction(point_exchange).mul(entry.points).toFraction(true)} {unit}</td>
+                </tr>
+            {/each}
+            </tbody>
+        </table>
     </div>
 </div>
